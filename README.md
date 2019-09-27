a3bf35235435a443cbd5a3b152e0826b72d3ab93
LHCortez <luizcquintas@gmail.com>
2019-09-26 22:23:07 -0300

    Adicionando task de start

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cotacoes
 Projeto de Cotações para estudo Node.js
+
+# Para executar configure as dependências:
+`npm i`
+
+# Depois:
+`npm start`
--- public/js/app.js
+++ public/js/app.js
@@ -23,7 +23,7 @@ cotacoesForm.addEventListener('submit', (event) => {
             }
 
 
-            fetch(`http://localhost:3000/cotacoes?ativo=${ativo}`).then((response) => {
+            fetch(`/cotacoes?ativo=${ativo}`).then((response) => {
                         response.json().then((data) => {
                                     if (data.error) {
                                         mainMessage.innerText = `Alguma coisa deu errado`
